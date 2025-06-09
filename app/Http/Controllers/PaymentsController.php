cac0cd73ea232fb23a29939d4d66d5fbfa2d13ef
Rahul Chouhan <r.chouhan64@gmail.com>
2025-06-09 18:45:50 +0530

    add payment method on affiliate

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
+use App\Models\PaymentMethods;
 use Illuminate\Http\Request;
 
 class PaymentsController extends Controller
@@ -11,8 +12,39 @@ class PaymentsController extends Controller
         return view('payments.index',compact('pageTitle'));
     }
 
-    public function paymentMethods(){
+    public function paymentMethods(Request $request){
         $pageTitle = 'Payment Methods';
-        return view('payments.methods',compact('pageTitle'));
+        $allPaymentMethods = PaymentMethods::where('user_id',auth()->user()->id)->get();
+        if($request->isMethod('post')){
+            if($request->rec_id>0){
+                $paymentMethods = PaymentMethods::where('id',$request->rec_id)->where('user_id',auth()->user()->id)->first();
+                if(empty($paymentMethods)){
+                    return redirect()->back()->with('error','Invalid Request');
+                }
+            }else{
+                $paymentMethods = new PaymentMethods();
+            }
+            $paymentMethods->user_id = auth()->user()->id; 
+            $paymentMethods->payment_method = $request->methods;
+            $paymentMethods->account_name = $request->account_name;
+            $paymentMethods->iban = $request->iban;
+            $paymentMethods->routing_number = $request->routing_number;
+            $paymentMethods->swift = $request->swift;
+            $paymentMethods->save();
+            if($request->rec_id>0){
+                return redirect()->back()->with('success','Payment Method Updated Successfully.');
+            }else{
+                return redirect()->back()->with('success','Payment Method Added Successfully.');
+            }
+        }
+        return view('payments.methods',compact('pageTitle','allPaymentMethods'));
+    }
+
+    public function updateMethodStatus(Request $request){
+        $record = PaymentMethods::find($request->updateid);
+        $record->status = ($record->status) ? 0 : 1;
+        $record->save();
+
+        return redirect()->back()->with('success','Status Updated');
     }
 }
